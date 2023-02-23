d18edaac8ac901a5b238fb899c7d3cfa47ed7d2c
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-23 10:37:09 -0600

    Initial project setup.

---
+++ MovieFetch/MovieFetch/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  MovieFetch
+//
+//  Created by iMac Pro on 2/23/23.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct MovieURLs {
+        static let movieBaseURL = "https://api.themoviedb.org/3/search/movie"
+    }
+    
+    struct PosterURLs {
+        static let posterBaseURL = "https://image.tmdb.org/t/p/w500"
+    }
+    
+}
